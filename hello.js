a2d3f2863ecd5ace83c4cfed96a13523e3ce488c
Miriam Diaz| <126723707+MiriamgDiaz@users.noreply.github.com>
2023-03-14 13:01:30 -0600

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,7 @@
+function hello(name) {
+    return "Hola " + name + "!"
+}
+
+console.log(hello ("MIRIAM"))
+console.log(hello ("MIguel"))
+console.log(hello (""))
---
+++ suma.js
@@ -0,0 +1,13 @@
+function suma (numero) {
+
+    var total = numero;
+
+    for(let i = 1; i < numero; i++) {
+        total = total + i;
+    }
+    return total
+}
+console.log(suma(4)); // 1 + 2 + 3 + 4 = 10
+console.log(suma(10)); // 55
+console.log(suma(15)); // 120
+
